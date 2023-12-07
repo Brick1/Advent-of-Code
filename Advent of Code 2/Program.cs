8abed19cc3bde09124e262665daef0c0c94b9a10
VašQe <v.komarek@post.cz>
2023-12-07 19:25:09 +0100

    Added second day solution

---
+++ Advent of Code 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.ExceptionServices;
+
+namespace Advent_of_Code_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArray = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "input.txt"));
+            int sum = 0;
+
+            foreach (string input in inputArray)
+            {
+                bool isFirst = true;
+                char last = '0';
+                char first = '0';
+                foreach (char c in input)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        if (isFirst)
+                        {
+                            first = c;
+                            isFirst = false;
+                        }
+                        last = c;
+                    }
+                }
+                sum += int.Parse(new char[] { first, last });
+            }
+
+            Console.WriteLine("Sum is: " + sum);
+        }
+    }
+}
